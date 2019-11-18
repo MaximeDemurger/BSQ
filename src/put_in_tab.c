7b91a10dda127460f01f7fde8f3254ec91fa5745
Maxime Demurger <maxime.demurger@epitech.eu>
2019-11-18 10:59:47 +0000

    put in tab

--- src/put_in_tab.c
+++ src/put_in_tab.c
@@ -10,9 +10,9 @@
 char **put_in_tab(int fd)
 {
     char **tab = NULL;
-    char buffer[100000 + 1];
+    char buffer[1000000 + 1];
 
-    read(fd, buffer, 100000);
+    read(fd, buffer, 1000000);
     tab = my_str_to_word_array(buffer, '\n');
     return tab;
 }
